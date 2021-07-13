a7afac29752b5e7f0f5e8a6229270c0949f1ad29
geql <geqilong1990@163.com>
2021-07-13 22:04:04 +0800

    v1.1 Copied codes from Ma, config adjustments for local startup

--- version1/shopizer/sm-shop/src/main/java/com/salesmanager/shop/tags/CustomResponseHeadersTag.java
+++ version1/shopizer/sm-shop/src/main/java/com/salesmanager/shop/tags/CustomResponseHeadersTag.java
@@ -14,21 +14,33 @@ import java.io.IOException;
 public class CustomResponseHeadersTag extends SimpleTagSupport {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomResponseHeadersTag.class);
 
-    private final String ATTRIBUTE1 = "attribute1";
-    private final String ATTRIBUTE2 = "attribute2";
-    private final String ATTRIBUTE3 = "attribute3";
+    private String attribute1 = "attribute1";
+    private String attribute2 = "attribute2";
+    private String attribute3 = "attribute3";
 
     @Override
     public void doTag() throws JspException, IOException {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        Object attr1Obj = request.getAttribute(ATTRIBUTE1);
-        Object attr2Obj = request.getAttribute(ATTRIBUTE1);
-        Object attr3Obj = request.getAttribute(ATTRIBUTE1);
+        Object attr1Obj = request.getAttribute(attribute1);
+        Object attr2Obj = request.getAttribute(attribute2);
+        Object attr3Obj = request.getAttribute(attribute3);
         LOGGER.info("attribute1:{},attribute2:{},attribute3:{}", attr1Obj, attr2Obj, attr3Obj);
         HttpServletResponse response = attributes.getResponse();
-        response.setHeader(ATTRIBUTE1, attr1Obj == null ? "" : attr1Obj.toString());
-        response.setHeader(ATTRIBUTE2, attr1Obj == null ? "" : attr2Obj.toString());
-        response.setHeader(ATTRIBUTE3, attr1Obj == null ? "" : attr3Obj.toString());
+        response.setHeader(attribute1, attr1Obj == null ? "" : attr1Obj.toString());
+        response.setHeader(attribute2, attr1Obj == null ? "" : attr2Obj.toString());
+        response.setHeader(attribute3, attr1Obj == null ? "" : attr3Obj.toString());
+    }
+
+    public void setAttribute1(String attribute1) {
+        this.attribute1 = attribute1;
+    }
+
+    public void setAttribute2(String attribute2) {
+        this.attribute2 = attribute2;
+    }
+
+    public void setAttribute3(String attribute3) {
+        this.attribute3 = attribute3;
     }
 }
